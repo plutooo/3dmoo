6788276995d8c3851bde03c37c37b6422becaa77
Normmatt <normmatt234@gmail.com>
2014-12-17 15:01:39 +1300

    Fix SXTAH.

--- src/arm11/armemu.c
+++ src/arm11/armemu.c
@@ -6154,7 +6154,7 @@ L_stm_s_takeabort:
 			if (ror == -1)
 				break;
 
-			Rm = ((state->Reg[BITS(0, 3)] >> ror) & 0xFFFF);
+            Rm = ((state->Reg[BITS(0, 3)] >> ror) & 0xFFFF) | ((state->Reg[BITS(0, 3)] << (32 - ror)) & 0xFFFF) & 0xFFFF;
 			if (Rm & 0x8000)
 				Rm |= 0xffff0000;
 

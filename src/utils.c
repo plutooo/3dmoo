3fffa2429a84439e53467e15e6117be4395e8c67
Normmatt <normmatt234@gmail.com>
2014-12-26 14:03:54 +1300

    Improve Float24 to Float32 conversion.

--- src/utils.c
+++ src/utils.c
@@ -1,14 +1,26 @@
 #include "util.h"
 
-float f24to32(u32 data, void *dataa) //this is not 100% correct fix it
-{
-    u32 dataout = (data << 8) &0x80000000; //Sign bit
-    dataout |= (data << 7)&~0xFF800000;
-    u32 exponent = (data >> 16) & 0x7F;
-    if (exponent != 0) {
-        exponent += 0x40;
+void f24to32(u32 data, void *dataa)
+    u32 sign = data & 0x800000;
+    int exp = (int)((data & 0x7F0000) >> 16);
+    u32 fraction = (data & 0xFFFF) << (23 - 16);
+
+    u32 out = 0;
+    out |= (sign != 0) ? 0x80000000 : 0;
+
+    if((data & ~0x800000) == 0)
+    {
+        exp = 0;
     }
-    dataout |= exponent << 23;
-    *(u32*)dataa = dataout;
-    return *(float*)&dataout;
+    else
+    {
+        exp = exp - 63 + 127;
+    }
+
+    // Make sure faction doesn't overflow
+    out |= fraction & 0x007FFFFF;
+    out |= ((u32)exp & 0xFF) << 23;
+
+    if(dataa != NULL)
+        *(u32*)dataa = out;
 }
